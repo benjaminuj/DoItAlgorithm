0b82031663e3b8f45c769ab1dec89bde1decc514
benjaminuj <chosj1526@gmail.com>
2022-11-25 23:43:13 +0900

    feat: [5번 문제] 합 배열 구하기

--- src/chap03/remainderSum05.java
+++ src/chap03/remainderSum05.java
@@ -13,12 +13,20 @@ public class remainderSum05 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		a.add(0);
+		sumArr.add(0);
+		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<N; i++) {
+		for(int i = 1; i<N+1; i++) {
 			a.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		for(int i = 1; i< N+1; i++) {
+			int temp = sumArr.get(i-1) +a.get(i);
+			sumArr.add(temp);
 		}
 
 	}
