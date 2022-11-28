38bed36ee739d324bd32f2a2512a60b99fe19522
benjaminuj <chosj1526@gmail.com>
2022-11-28 23:41:38 +0900

    feat: [7번 문제] 기본 세팅

---
+++ src/chap03/jumongsCommand07.java
@@ -0,0 +1,15 @@
+package chap03;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class jumongsCommand07 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+	}
+
+}
