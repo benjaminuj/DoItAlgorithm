32181fcfad59b5da53137790b1c2588517f6203b
benjaminuj <chosj1526@gmail.com>
2022-11-29 23:54:23 +0900

    feat: [7번 문제] 입력받기

--- src/chap03/jumongsCommand07.java
+++ src/chap03/jumongsCommand07.java
@@ -8,8 +8,15 @@ public class jumongsCommand07 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		int[] A = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		for(int i =0; i<N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+	
 	}
 
 }
