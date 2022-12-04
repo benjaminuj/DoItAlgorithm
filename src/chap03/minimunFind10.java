3ed933a547cffdf80bbcafd72db32eac6244067f
benjaminuj <chosj1526@gmail.com>
2022-12-04 15:29:20 +0900

    feat: [10번 문제] 입력받기

---
+++ src/chap03/minimunFind10.java
@@ -0,0 +1,16 @@
+package chap03;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class minimunFind10 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+	}
+}
