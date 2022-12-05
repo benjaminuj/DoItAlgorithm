ead45f86cd08dc26f6715ea0a5ab88a36c63e124
benjaminuj <chosj1526@gmail.com>
2022-12-05 20:26:52 +0900

    feat: [10번 문제] N개의 수 입력받기

--- src/chap03/minimunFind10.java
+++ src/chap03/minimunFind10.java
@@ -12,5 +12,16 @@ public class minimunFind10 {
 		
 		int N = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
+		int[] A = new int[N];
+		
+		st = new StringTokenizer(bf.readLine());
+		for(int i =0 ; i<N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i =0 ; i<N; i++) {
+			System.out.println(A[i]);
+		}
+		
 	}
 }
