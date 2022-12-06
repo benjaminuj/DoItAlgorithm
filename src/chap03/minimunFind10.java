01d979f02e038cc1d12fbcfb7e6508f710668392
benjaminuj <chosj1526@gmail.com>
2022-12-06 22:50:06 +0900

    feat: [10번 문제] 덱 이용한 최솟값 선정 및 출력

--- src/chap03/minimunFind10.java
+++ src/chap03/minimunFind10.java
@@ -5,26 +5,37 @@ import java.util.StringTokenizer;
 import java.io.IOException;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class minimunFind10 {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
-		int[] A = new int[N];
-		
-		st = new StringTokenizer(bf.readLine());
+		st = new StringTokenizer(br.readLine());
+		Deque<Node> myDeque = new LinkedList<>();
 		for(int i =0 ; i<N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+			int now = Integer.parseInt(st.nextToken());
+			
+			while(!myDeque.isEmpty() && myDeque.getLast().value > now) {
+				myDeque.removeLast();
+			}
+			
+			myDeque.addLast(new Node(now, i));
+			if(myDeque.getFirst().index <= i-L) {
+				myDeque.removeFirst();
+			}
+			bw.write(myDeque.getFirst().value + " ");
 		}
-		
-		Deque<Node> checkDeque = new LinkedList<>();
-		
+		bw.flush();
+		bw.close();
 	
-		
 	}
 	
 	static class Node {
