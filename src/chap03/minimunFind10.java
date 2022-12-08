318bcac99c619da7a4d5efca921ff838008b9656
benjaminuj <chosj1526@gmail.com>
2022-12-08 23:50:31 +0900

    feat: [11번 문제] 문제 요구 기능

--- src/chap03/minimunFind10.java
+++ src/chap03/minimunFind10.java
@@ -9,24 +9,19 @@ import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 
 public class minimunFind10 {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		int N = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
 		Deque<Node> myDeque = new LinkedList<>();
 		for(int i =0 ; i<N; i++) {
 			int now = Integer.parseInt(st.nextToken());
-			
 			while(!myDeque.isEmpty() && myDeque.getLast().value > now) {
 				myDeque.removeLast();
 			}
-			
 			myDeque.addLast(new Node(now, i));
 			if(myDeque.getFirst().index <= i-L) {
 				myDeque.removeFirst();
@@ -35,13 +30,10 @@ public class minimunFind10 {
 		}
 		bw.flush();
 		bw.close();
-	
 	}
-	
 	static class Node {
 		public int value;
-		public int index;
-		
+		public int index;	
 		Node(int value, int index) {
 			this.value = value;
 			this.index = index;
--- src/chap03/stackAscendingSort11.java
+++ src/chap03/stackAscendingSort11.java
@@ -1,16 +1,48 @@
 package chap03;
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.ArrayList;
 
 public class stackAscendingSort11 {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
+		int[] arr = new int[n];
+		ArrayList<String> arrList = new ArrayList<>();
+		Stack<Integer> stack = new Stack<>();
 		for(int i =0; i<n; i++) {
-			int num = sc.nextInt();
+			arr[i] = sc.nextInt();
 		}
-
+		int now = 1;
+		int index=0;
+		while(index < n) {
+			if(arr[index] >= now) {
+		    	stack.push(now);
+		    	arrList.add("+");
+		        if(arr[index] == now) {
+		        	stack.pop();
+		        	arrList.add("-");
+		        	index++;
+		        }
+		    	now++;
+			}
+			if(index < n) {
+				if(arr[index] < now) {
+			    	if(stack.peek() == arr[index]) {
+			    		stack.pop();
+			    		arrList.add("-");
+			    		index++;
+			    	}
+			        else {
+			        	System.out.println("NO");
+			        	return;
+			        }
+			    }
+			}	
+		}
+		for(int i=0; i<arrList.size(); i++) {
+			System.out.println(arrList.get(i));
+		}
+		return;
 	}
-
 }
